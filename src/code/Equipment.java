52482a415b009ed9a702389efbb270ef946a61ad
MShizikU <sidorovstasd7@gmail.com>
2023-10-08 13:01:21 +0300

    inital

---
+++ src/code/Equipment.java
@@ -0,0 +1,12 @@
+package code;
+
+import java.util.ArrayList;
+
+public class Equipment {
+
+    ArrayList<Character> hiddenWord;
+    ArrayList<Character> alphabet;
+
+    ArrayList<Character> userWord;
+
+}
